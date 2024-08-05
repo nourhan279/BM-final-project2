cceabf81212996d891dc65f91e22e53c749acdae
nourhan mohamed <nourhanmo279@gmail.com>
2024-08-05 20:38:33 +0300

    update profile

---
+++ src/app/auth.service.ts
@@ -0,0 +1,62 @@
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class AuthService {
+//   private apiUrl = 'http://localhost:3000';
+
+//   constructor(private http: HttpClient) {}
+
+//   ggetUser(): Observable<any> {
+//     return this.http.get(`${this.apiUrl}/user`);
+//   }
+
+//   updateUser(updatedFields: any): Observable<any> {
+//     return this.http.put(`${this.apiUrl}/user/4299`, updatedFields);
+//   }
+
+//   verifyCurrentPassword(password: string): Observable<any> {
+//     return this.http.post(`${this.apiUrl}/verify-password`, { password });
+//   }
+
+//   updatePassword(passwordData: {
+//     currentPassword: string;
+//     newPassword: string;
+//   }): Observable<any> {
+//     return this.http.put(`${this.apiUrl}/password`, passwordData);
+//   }
+// }
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) {}
+
+  getUser(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/user`);
+  }
+
+  updateUser(updatedFields: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/user/4299`, updatedFields);
+  }
+
+  verifyCurrentPassword(password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/verify-password`, { password });
+  }
+
+  updatePassword(passwordData: {
+    currentPassword: string;
+    newPassword: string;
+  }): Observable<any> {
+    return this.http.put(`${this.apiUrl}/password`, passwordData);
+  }
+}
